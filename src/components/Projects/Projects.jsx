1d34f007942b887b6b485ee4e03035f1217232d4
Blissmal <malutibethuel@gmail.com>
2025-03-13 21:40:35 +0300

    Added new project card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import blisSocial from '../../Assets/Projects/bliss-socially.png'
 import fbLIke from '../../Assets/Projects/bliss-social.png'
+import firebaseChat from "../../Assets/Projects/firebaseChat.png"
 
 const Projects = () => {
   return (
@@ -36,6 +37,17 @@ const Projects = () => {
               description="A simple facebook lookalike with clerk authentication, social post creation with image upload using cloudinary, postgres database from neon to store user and other tables information and status creation but still one status at a time. The code also protects the main route and redirects unauthorized users to the login page if they are not authenticated"
               ghLink="https://github.com/Blissmal/next-social-media-app"
               demoLink="https://bliss-social.vercel.app"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={firebaseChat}
+              isBlog={false}
+              title="React firebase chat"
+              description="A react firebase chat application with signup and login functionalities using firebaseAuth and images upload to firebase firestore. The app allows seamless chatting including emoji and images transfer for more interactivity. It has block functionality where blocked users are not able to view the chat component of the user"
+              ghLink="https://github.com/Blissmal/react-firebaseChat"
+              demoLink="https://react-firebase-chat-eight-delta.vercel.app/"
             />
           </Col>
         </Row>
